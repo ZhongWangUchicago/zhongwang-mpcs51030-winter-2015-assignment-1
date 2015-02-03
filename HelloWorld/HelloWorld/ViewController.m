f43704110fc55611f2c88e625f634d183c5aa569
zhongwang <zhongwang@uchicago.edu>
2015-02-03 12:56:40 -0600

    1
    ass1

---
+++ HelloWorld/HelloWorld/ViewController.m
@@ -0,0 +1,42 @@
+//
+//  ViewController.m
+//  HelloWorld
+///Users/zhong/Desktop/HelloWorld/HelloWorld/ViewController.h
+//  Created by WangZhong on 15-1-12.
+//  Copyright (c) 2015年 Zhong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+@synthesize userName = _userName;
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)changeGreeting:(id)sender {
+    self.userName = self.textField.text;
+    NSString *nameString = self.userName;
+    if ([nameString length] == 0) {
+        nameString = @"World";
+    }
+    NSString *greeting = [[NSString alloc] initWithFormat:@"Hello, %@!", nameString];
+    self.label.text = greeting;
+}
+- (BOOL)textFieldShouldReturn:(UITextField *)theTextField {
+    if (theTextField == self.textField) {
+        [theTextField resignFirstResponder];
+    }
+    return YES; }
+
+@end
